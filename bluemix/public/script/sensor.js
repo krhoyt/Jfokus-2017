8a149b5fba856b268611264f72664704312d4646
Kevin Hoyt <parkerkrhoyt@gmail.com>
2017-01-27 15:40:47 -0700

    Added media queries.
    Display works consistently in both portrait and landscape modes.
    Showing orientation in alpha, beta, and gamma.

---
+++ bluemix/public/script/sensor.js
@@ -0,0 +1,8 @@
+class Sensor {
+
+  constructor() {
+    this._portrait = document.querySelector( 'iot-orientation:first-of-type' );
+    this._orientation = document.querySelector( 'iot-orientation:last-of-type' );
+  }
+	
+}
