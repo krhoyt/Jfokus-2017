a81759c22d41bc53100aef0fdf813151995587c5
Kevin Hoyt <parkerkrhoyt@gmail.com>
2017-02-01 08:21:14 -0700

    Tuning stream report.
    Somewhere along the way, my orientation reporting code slipped back to
    1:0 (portrait:landscape). Fixed to be 100:0. Updated places where it is
    reported.

--- bluemix/public/script/sensor.js
+++ bluemix/public/script/sensor.js
@@ -1,11 +1,11 @@
 class Sensor {
 
   constructor() {
+    this._alpha = 0;
+    this._beta = 0;    
+    this._gamma = 0;
     this._portrait = document.querySelector( 'iot-orientation:first-of-type' );
-    this._orientation = document.querySelector( 'iot-orientation:last-of-type' );
-    this._alpha = document.querySelector( 'iot-gauge:nth-of-type( 1 )' );
-    this._beta = document.querySelector( 'iot-gauge:nth-of-type( 2 )' );
-    this._gamma = document.querySelector( 'iot-gauge:nth-of-type( 3 )' );
+    this._landscape = document.querySelector( 'iot-orientation:last-of-type' );
     this._mode = 1;
     this._client = uuid.v4();
 
@@ -14,12 +14,14 @@ class Sensor {
       8883, 
       'a:' + Sensor.IOT_ORGANIZATION + ':' + this._client
     );
+    this._socket.onMessageArrived = this.doMessage.bind( this );
     this._socket.connect( {
       userName: Sensor.IOT_USERNAME,
       password: Sensor.IOT_PASSWORD,
       useSSL: true,
       onSuccess: function() {
         console.log( 'Connected.' );
+        this._socket.subscribe( Sensor.IOT_COMMAND );        
         this._interval = setInterval( this.report.bind( this ), 1000 );        
       }.bind( this )
     } );
@@ -27,37 +29,59 @@ class Sensor {
     window.addEventListener( 'deviceorientation', evt => this.doOrientation( evt ) );     
   }
 	
+  map( x, in_min, in_max, out_min, out_max ) {
+    return ( x - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
+  } 
+
   report() {
     var message = null;
 
-    console.log( this._alpha.value + ', ' + this._beta.value + ', ' + this._gamma.value );
+    console.log( this._alpha + ', ' + this._beta + ', ' + this._gamma );
 
     message = new Paho.MQTT.Message( JSON.stringify( {
       ts: new Date().toISOString(),
       d: {
-        x: this._alpha.value,
-        y: this._beta.value,
-        z: this._gamma.value,
+        x: this._alpha,
+        y: this._beta,
+        z: this._gamma,
         mode: this._mode
       }
     } ) );
-    message.destinationName = Sensor.IOT_DESTINATION;
+    message.destinationName = Sensor.IOT_EVENT;
     this._socket.send( message );   
   }
 
+  doMessage( message ) {
+    var data = null;
+
+    data = JSON.parse( message.payloadString );
+    console.log( data );
+
+    this._portrait.percentage = data.d.avgMode;
+    this._landscape.percentage = 100 - data.d.avgMode;
+  }
+
   doOrientation( evt ) {
-    this._alpha.value = evt.alpha;
-    this._beta.value = evt.beta;
-    this._gamma.value = evt.gamma;
+    this._alpha = evt.alpha;
+    this._beta = evt.beta;
+    this._gamma = evt.gamma;
 
     // Landscape == 0
-    // Portrait == 1    
-    this._mode = document.body.clientWidth > document.body.clientHeight ? 0 : 1;   
+    // Portrait == 100    
+    this._mode = document.body.clientWidth > document.body.clientHeight ? 0 : 100;   
   }
 
 }
 
-Sensor.IOT_DESTINATION = 'iot-2/type/Jfokus/id/Accelerometer/evt/sensorData/fmt/json';
+Sensor.ALPHA_MAX = 360;  
+Sensor.ALPHA_MIN = 0;
+Sensor.BETA_MAX = 180; 
+Sensor.BETA_MIN = -180;
+Sensor.GAMMA_MAX = 90; 
+Sensor.GAMMA_MIN = -90;
+
+Sensor.IOT_COMMAND = 'iot-2/type/Jfokus/id/Accelerometer/cmd/averages/fmt/json';
+Sensor.IOT_EVENT = 'iot-2/type/Jfokus/id/Accelerometer/evt/sensorData/fmt/json';
 Sensor.IOT_HOST = 'ts200f.messaging.internetofthings.ibmcloud.com';
 Sensor.IOT_ORGANIZATION = 'ts200f';
 Sensor.IOT_PASSWORD = 'lQWRcupAZqxi58A--e';
