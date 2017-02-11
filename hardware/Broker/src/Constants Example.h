8ccf65bd4065fd35e5839b4cbcfd3ddca30a3057
Kevin Hoyt <parkerkrhoyt@gmail.com>
2017-02-11 12:15:21 +0100

    Example configuration files.
    Details changed to protect the innocent.

---
+++ hardware/Broker/src/Constants Example.h
@@ -0,0 +1,8 @@
+namespace Constants
+{
+    char *IOT_CLIENT = "d:tsas5d:Photon:IBM";
+    char *IOT_HOST = "tsas5d.messaging.internetofthings.ibmcloud.com";
+    char *IOT_USERNAME = "use-token-auth";
+    char *IOT_PASSWORD = "Yy2GAlR7(JTtvZ_Kj0";
+    char *IOT_TOPIC = "iot-2/evt/stacks/fmt/json";
+}
---
+++ mobile/Bean/Constants Example.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct Constants {
+    static let IOT_ORGANIZATION = "tsas5d"
+    static let IOT_USERNAME = "a-tsas5d-oewg5805yp"
+    static let IOT_PASSWORD = "lQWR--AZqxi58Acue"
+    static let IOT_TOPIC = "iot-2/type/Bean/id/IBM/evt/stacks/fmt/json"
+}
